56e02bc60d89c4e12a4941d11b71193056619026
Will Hines <bhines7@gmail.com>
2020-11-12 15:56:38 -0600

    Basic Form Design and Hotkey Library

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.StepText = new System.Windows.Forms.TextBox();
+            this.ClueLabel = new System.Windows.Forms.Label();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StepText
+            // 
+            this.StepText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.StepText.Location = new System.Drawing.Point(27, 37);
+            this.StepText.Name = "StepText";
+            this.StepText.Size = new System.Drawing.Size(174, 20);
+            this.StepText.TabIndex = 0;
+            // 
+            // ClueLabel
+            // 
+            this.ClueLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ClueLabel.AutoSize = true;
+            this.ClueLabel.Location = new System.Drawing.Point(52, 21);
+            this.ClueLabel.Name = "ClueLabel";
+            this.ClueLabel.Size = new System.Drawing.Size(113, 13);
+            this.ClueLabel.TabIndex = 1;
+            this.ClueLabel.Text = "Enter Clue Instructions";
+            this.ClueLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // StartButton
+            // 
+            this.StartButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.StartButton.AutoSize = true;
+            this.StartButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.StartButton.Location = new System.Drawing.Point(76, 63);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(66, 23);
+            this.StartButton.TabIndex = 2;
+            this.StartButton.Text = "Start (F10)";
+            this.StartButton.UseVisualStyleBackColor = true;
+            // 
+            // StopButton
+            // 
+            this.StopButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.StopButton.AutoSize = true;
+            this.StopButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.StopButton.Location = new System.Drawing.Point(76, 92);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(66, 23);
+            this.StopButton.TabIndex = 3;
+            this.StopButton.Text = "Stop (F11)";
+            this.StopButton.UseVisualStyleBackColor = true;
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ResetButton.AutoSize = true;
+            this.ResetButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ResetButton.Location = new System.Drawing.Point(76, 121);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(72, 23);
+            this.ResetButton.TabIndex = 4;
+            this.ResetButton.Text = "Reset (F12)";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(236, 165);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.ClueLabel);
+            this.Controls.Add(this.StepText);
+            this.Name = "Form1";
+            this.Text = "Slider Solver";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox StepText;
+        private System.Windows.Forms.Label ClueLabel;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Button ResetButton;
     }
 }
 
